19f31787362853f43d2f7b130dd00f626a9c3819
ayu0616 <ayumuogawa0616@gmail.com>
2023-07-25 07:59:20 +0900

    main.cppは削除する（combinedだけ残す）

--- archives/abc/abc105/C/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "../../lib.hpp"
-
-// #define MOD 1000000007
-// #define MOD 998244353
-
-int main()
-{
-
-}
--- archives/others/dp/Q/main.cpp
+++
@@ -1,9 +0,0 @@
-K#include "../../lib.hpp"
-
-// #define MOD 1000000007
-// #define MOD 998244353
-
-int main()
-{
-    
-}
