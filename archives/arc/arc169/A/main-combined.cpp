cd55da663cc5d6c099d0205f6ae2ac6e3a2ee04d
ayu0616 <ayumuogawa0616@gmail.com>
2023-12-09 23:39:00 +0900

    archived: arc169

---
+++ archives/arc/arc169/A/main-combined.cpp
@@ -0,0 +1,578 @@
+#line 1 "lib.hpp"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// 型エイリアス
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vs = vector<string>;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+
+// マクロ
+#define ov3(a, b, c, name, ...) name
+#define rep2(i, a, b) for (ll i = (a); i < (b); i++)
+#define rep1(i, n) rep2(i, 0, n)
+#define rep0(n) rep1(iiiii, n)
+#define rep(...) ov3(__VA_ARGS__, rep2, rep1, rep0)(__VA_ARGS__)
+#define foreach(v, c) for (auto &v : c)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define debug(x) cerr << #x << ": " << x << '\n'
+
+// 移動
+int dx4[4] = {1, 0, -1, 0};                // 4方向移動用
+int dy4[4] = {0, 1, 0, -1};                // 4方向移動用
+int dx8[8] = {1, 1, 0, -1, -1, -1, 0, 1};  // 8方向移動用
+int dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};  // 8方向移動用
+
+// 最大値
+constexpr int INT_INF = 1 << 30;  // intの最大値
+constexpr ll LL_INF = 1LL << 60;  // llの最大値
+
+// @brief Yes/Noを出力する
+// @param b 条件
+// @param yes 出力する文字列（デフォルトは`Yes`）
+// @param no 出力する文字列（デフォルトは`No`）
+inline bool yes_no(bool b, string yes = "Yes", string no = "No") {
+    cout << (b ? yes : no) << endl;
+    return b;
+}
+
+// @brief ヒープ（小さい順）
+template <typename T>
+class greater_heap {
+   private:
+    priority_queue<T, vector<T>, greater<T>> q;
+
+   public:
+    greater_heap() : q() {}
+
+    bool not_empty() { return q.empty(); }
+
+    void print_all() {
+        while (not_empty()) {
+            cout << q.top() << endl;
+            q.pop();
+        }
+    }
+
+    void push(T x) { return q.push(x); }
+
+    void pop() { return q.pop(); }
+
+    T top() { return q.top(); }
+};
+
+// @brief 大きい方で更新
+// @param a 更新される値
+// @param b 更新する値
+template <typename T>
+inline bool chmax(T &a, const T &b) {
+    return a < b ? a = b, true : false;
+}
+
+// @brief 小さい方で更新
+// @param a 更新される値
+// @param b 更新する値
+template <typename T>
+inline bool chmin(T &a, const T &b) {
+    return a > b ? a = b, true : false;
+}
+
+// 可変長引数のmin関数
+template <class... T>
+constexpr auto min(T... a) {
+    return min(initializer_list<common_type_t<T...>>{a...});
+}
+
+// vectorを引数に取るmin関数
+template <class T>
+constexpr auto min(vector<T> a) {
+    return *min_element(a.begin(), a.end());
+}
+
+// setを引数に取るmin関数
+template <class T>
+constexpr auto min(set<T> a) {
+    return *min_element(a.begin(), a.end());
+}
+
+// 可変長引数のmax関数
+template <class... T>
+constexpr auto max(T... a) {
+    return max(initializer_list<common_type_t<T...>>{a...});
+}
+
+// vectorを引数に取るmax関数
+template <class T>
+constexpr auto max(vector<T> a) {
+    return *max_element(a.begin(), a.end());
+}
+
+// setを引数に取るmax関数
+template <class T>
+constexpr auto max(set<T> a) {
+    return *max_element(a.begin(), a.end());
+}
+
+struct Edge;
+
+// @brief グラフの頂点
+struct Vertex {
+    int id;
+    vector<Edge> edges;
+
+    Vertex(int id) {
+        this->id = id;
+        this->edges = vector<Edge>();
+    }
+
+    operator int() const { return this->id; }
+};
+
+// @brief グラフの辺
+// @param to 辺の行き先
+// @param cost 辺のコスト
+struct Edge {
+    Vertex *from, *to;
+    ll cost;
+
+    Edge(int from, int to, ll cost = 1) {
+        this->from = new Vertex(from);
+        this->to = new Vertex(to);
+        this->cost = cost;
+    }
+
+    Edge(Vertex *from, Vertex *to, ll cost = 1) {
+        this->from = from;
+        this->to = to;
+        this->cost = cost;
+    }
+
+    operator int() const { return this->to->id; }
+};
+
+// @brief グラフ
+class Graph : vector<Vertex> {
+    int n;
+
+   public:
+    Graph(int n) {
+        this->n = n;
+        rep(i, n) this->push_back(Vertex(i));
+    }
+
+    inline void add_edge(int from, int to, ll cost = 1) {
+        Vertex &u = this->at(from);
+        Vertex &v = this->at(to);
+        Edge e(u, v, cost);
+        u.edges.push_back(e);
+    }
+
+    // @brief ダイクストラ法
+    // @param start 始点
+    // @return 始点からの最短距離（到達不可能な点への距離は-1とする）
+    vl dyjkstra(int start) {
+        vl dist(n, LLONG_MAX);
+        dist[start] = 0;
+        priority_queue<pll, vpll, greater<pll>> que;
+        que.push({0, start});
+        while (!que.empty()) {
+            pll p = que.top();
+            que.pop();
+            int v = p.second;
+            if (dist[v] < p.first) continue;
+            for (auto e : this->at(v).edges) {
+                if (dist[*e.to] > dist[v] + e.cost) {
+                    dist[*e.to] = dist[v] + e.cost;
+                    que.push({dist[*e.to], *e.to});
+                }
+            }
+        }
+        rep(i, n) if (dist[i] == LLONG_MAX) dist[i] = -1;
+        return dist;
+    }
+
+    // @brief ワーシャルフロイド法
+    // @return 全点間の最短距離（到達不可能な点への距離は-1とする）
+    vvl warshall_floyd() {
+        vvl dist(n, vl(n, LLONG_MAX));
+        rep(i, n) dist[i][i] = 0;
+        rep(i, n) {
+            for (auto e : this->at(i).edges) {
+                dist[i][*e.to] = e.cost;
+            }
+        }
+        rep(k, n) {
+            rep(i, n) {
+                rep(j, n) {
+                    if (dist[i][k] != LLONG_MAX && dist[k][j] != LLONG_MAX) {
+                        chmin(dist[i][j], dist[i][k] + dist[k][j]);
+                    }
+                }
+            }
+        }
+        rep(i, n) rep(j, n) if (dist[i][j] == LLONG_MAX) dist[i][j] = -1;
+        return dist;
+    }
+
+    // @brief ベルマンフォード法
+    // @param start 始点
+    // @return 始点からの最短距離（到達不可能な点への距離は-1とする）
+    vl bellman_ford(int start) {
+        vl dist(n, LLONG_MAX);
+        dist[start] = 0;
+        rep(i, n) {
+            rep(j, n) {
+                for (auto e : this->at(j).edges) {
+                    if (dist[j] != LLONG_MAX && dist[*e.to] > dist[j] + e.cost) {
+                        dist[*e.to] = dist[j] + e.cost;
+                        if (i == n - 1) dist[*e.to] = LLONG_MIN;
+                    }
+                }
+            }
+        }
+        rep(i, n) if (dist[i] == LLONG_MAX) dist[i] = -1;
+        return dist;
+    }
+
+    // @brief トポロジカルソート
+    // @return トポロジカルソートされた頂点のリスト
+    vi topological_sort() {
+        vi res;
+        vb used(n, false);
+        function<void(int)> dfs = [&](int v) {
+            used[v] = true;
+            for (auto e : this->at(v).edges) {
+                if (!used[*e.to]) dfs(*e.to);
+            }
+            res.push_back(v);
+        };
+        rep(i, n) {
+            if (!used[i]) dfs(i);
+        }
+        reverse(all(res));
+        return res;
+    }
+
+    vector<Edge> operator[](int id) { return this->at(id).edges; }
+};
+
+/* 素数判定 */
+constexpr bool is_prime(const ll n) {
+    if (n <= 1) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
+    for (ll i = 3; i * i <= n; i += 2) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+/*
+@brief 素数を列挙する関数
+@param upper 素数の上限
+ */
+inline vector<ll> create_primes(const ll upper) {
+    vector<ll> primes;
+    vector<bool> is_p(upper + 1, true);
+
+    for (ll i = 2; i <= upper; i++) {
+        if (is_p[i]) {
+            primes.push_back(i);
+            for (ll j = i * 2; j <= upper; j += i) {
+                is_p[j] = false;
+            }
+        }
+    }
+    return primes;
+}
+
+/* @brief 素因数分解 */
+template <class T>
+constexpr vl prime_factorize(const T n) {
+    vl res;
+    while (n % 2 == 0) {
+        res.emplace_back(2);
+        n /= 2;
+    }
+    for (T i = 3; i * i <= n; i += 2) {
+        while (n % i == 0) {
+            res.emplace_back(i);
+            n /= i;
+        }
+    }
+    if (n != 1) {
+        res.emplace_back(n);
+    }
+    return res;
+}
+
+template <class T = int>
+class UnionFind {
+    vector<T> par, rank, siz;
+
+   public:
+    // @param n 要素数
+    inline UnionFind(T n) {
+        par = vector<T>(n, -1);
+        rank = vector<T>(n, 0);
+        siz = vector<T>(n, 1);
+    }
+
+    // xの根を求める
+    inline T root(T x) {
+        if (par[x] == -1)
+            return x;
+        else
+            return par[x] = root(par[x]);
+    }
+
+    // xとyが同じグループに属するかどうか（根が一致するかどうか）
+    inline bool same(T x, T y) { return root(x) == root(y); }
+
+    // xを含むグループとyを含むグループとを併合する
+    inline void unite(T x, T y) {
+        x = root(x);
+        y = root(y);
+        if (x == y) return;
+
+        if (rank[x] < rank[y]) swap(x, y);
+        par[y] = x;
+        if (rank[x] == rank[y]) rank[x]++;
+        siz[x] += siz[y];
+    }
+
+    // xを含むグループのサイズ
+    inline T size(T x) { return siz[root(x)]; }
+
+    // グループ数
+    inline T groups() {
+        T res = 0;
+        for (T i = 0; i < par.size(); i++) {
+            if (par[i] == -1) res++;
+        }
+        return res;
+    }
+};
+
+/*
+自動的にmodを取ってくれる型
+使い方：static_modint<mod> a;
+ただし、modは素数でなければならない
+*/
+template <int mod>
+class static_modint {
+    ll x;
+
+   public:
+    inline static_modint(ll x = 0) : x((x % mod + mod) % mod) {}
+    inline static_modint operator-() const { return static_modint(-x); }
+    inline static_modint &operator+=(const static_modint &a) {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+    inline static_modint &operator-=(const static_modint &a) {
+        if ((x += mod - a.x) >= mod) x -= mod;
+        return *this;
+    }
+    inline static_modint &operator*=(const static_modint &a) {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+    inline static_modint operator+(const static_modint &a) const {
+        static_modint res(*this);
+        return res += a;
+    }
+    inline static_modint operator-(const static_modint &a) const {
+        static_modint res(*this);
+        return res -= a;
+    }
+    inline static_modint operator*(const static_modint &a) const {
+        static_modint res(*this);
+        return res *= a;
+    }
+    // 二分累乗法によるべき乗
+    inline static_modint pow(ll t) const {
+        if (!t) return 1;
+        static_modint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+    // 逆元を求める
+    inline static_modint inv() const { return pow(mod - 2); }
+    inline static_modint &operator/=(const static_modint &a) { return (*this) *= a.inv(); }
+    inline static_modint operator/(const static_modint &a) const {
+        static_modint res(*this);
+        return res /= a;
+    }
+
+    friend ostream &operator<<(ostream &os, const static_modint &m) {
+        os << m.x;
+        return os;
+    }
+};
+
+// 桁和
+template <class T>
+constexpr int digit_sum(T n) {
+    int res = 0;
+    while (n > 0) {
+        res += n % 10;
+        n /= 10;
+    }
+    return res;
+}
+
+template <class T>
+inline istream &operator>>(istream &is, vector<T> &v) {
+    for (auto &i : v) is >> i;
+    return is;
+}
+
+template <class T>
+inline ostream &operator<<(ostream &os, const vector<T> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        os << v[i];
+        if (i != v.size() - 1) os << " ";
+    }
+    return os;
+}
+
+template <class T>
+inline ostream &operator<<(ostream &os, const vector<vector<T>> &v) {
+    for (int i = 0; i < v.size(); i++) {
+        os << v[i];
+        if (i != v.size() - 1) os << endl;
+    }
+    return os;
+}
+
+template <class T>
+inline ostream &operator<<(ostream &os, set<T> &s) {
+    ll i = 0;
+    for (T v : s) {
+        cout << v;
+        if (i++ < s.size() - 1) os << " ";
+    }
+    return os;
+}
+
+template <class T>
+inline ostream &operator<<(ostream &os, priority_queue<T> q) {
+    ll i = 0;
+    while (!q.empty()) {
+        cout << q.top();
+        q.pop();
+        if (i++ < q.size() - 1) os << " ";
+    }
+    return os;
+}
+template <class T>
+inline ostream &operator<<(ostream &os, greater_heap<T> q) {
+    ll i = 0;
+    while (!q.empty()) {
+        cout << q.top();
+        q.pop();
+        if (i++ < q.size() - 1) os << " ";
+    }
+    return os;
+}
+
+template <class T, class S>
+inline ostream &operator<<(ostream &os, pair<T, S> p) {
+    os << p.first << " " << p.second;
+    return os;
+}
+
+template <typename T>
+inline void print(T v) {
+    cout << v << endl;
+}
+
+void dfs_combination(int n, int r, int from, vi &use, vvi &uses) {
+    if (from + r > n) {
+        return;
+    } else if (r == 0) {
+        uses.push_back(use);
+        return;
+    }
+    use.push_back(from);
+    dfs_combination(n, r - 1, from + 1, use, uses);
+    use.pop_back();
+    dfs_combination(n, r, from + 1, use, uses);
+}
+
+// @brief 組み合わせを全列挙する
+template <class T>
+vector<vector<T>> combinations(vector<T> &v, int r) {
+    int n = v.size();
+    vi use;
+    vvi uses;
+    dfs_combination(n, r, 0, use, uses);
+    vector<vector<T>> res(uses.size());
+    rep(i, uses.size()) {
+        vi &u = uses[i];
+        vector<T> tmp;
+        for (auto &i : u) {
+            tmp.push_back(v[i]);
+        }
+        res[i] = tmp;
+    }
+    return res;
+}
+#line 2 "arc169/A/main.cpp"
+
+// constexpr int MOD = 998244353;
+// constexpr int MOD = 1000000007;
+// constexpr int MOD = 1000000009;
+
+// using mint = static_modint<MOD>;
+
+int main() {
+    int N;
+    cin >> N;
+    vl A(N), P(N - 1);
+    cin >> A >> P;
+    rep(i, N - 1) P[i]--;
+    Graph g(N);
+    rep(i, 1, N) { g.add_edge(P[i - 1], i); }
+    vvi depth(N);
+    int max_depth = 0;
+    auto dfs = [&](auto &&f, int cur, int par, int d) -> void {
+        max_depth = max(max_depth, d);
+        depth[cur].push_back(d);
+        for (auto &&e : g[cur]) {
+            if (*e.to == par) continue;
+            f(f, *e.to, cur, d + 1);
+        }
+    };
+    dfs(dfs, 0, -1, 0);
+    ll s = A[0];
+    rep(i, 1, N) rep(j, depth[i].size()) { s += A[i] * (max_depth - depth[i][j]); }
+    ll ans = 0;
+    rep(i, 1, N) { ans += (ll)depth[i].size() * A[i]; }
+    if (ans > 0)
+        cout << '+';
+    else if (ans < 0)
+        cout << '-';
+    else {
+        if (s > 0)
+            cout << '+';
+        else if (s < 0)
+            cout << '-';
+        else
+            cout << '0';
+    }
+    cout << endl;
+}
