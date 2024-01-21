087570db61287bd4ac9b38a43937646963e97b93
ayu0616 <ayumuogawa0616@gmail.com>
2024-01-21 22:13:15 +0900

    典型90・013を解いた

--- typical90/013/main.cpp
+++ typical90/013/main.cpp
@@ -6,4 +6,18 @@
 
 // using mint = static_modint<MOD>;
 
-int main() {}
+int main() {
+    int N, M;
+    cin >> N >> M;
+    Graph g(N);
+    while (M--) {
+        int A, B, C;
+        cin >> A >> B >> C;
+        A--, B--;
+        g.add_edge(A, B, C);
+        g.add_edge(B, A, C);
+    }
+    vl from_zero = g.dyjkstra(0);
+    vl from_n = g.dyjkstra(N - 1);
+    rep(k, N) { cout << from_zero[k] + from_n[k] << endl; }
+}
