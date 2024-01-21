d5766e88e7d2000dae9238ee5a2f03b1aa01e875
ayu0616 <ayumuogawa0616@gmail.com>
2024-01-21 22:09:54 +0900

    典型90のファイルを作成した

---
+++ typical90/046/main.cpp
@@ -0,0 +1,9 @@
+#include "../../lib.hpp"
+
+// constexpr int MOD = 998244353;
+// constexpr int MOD = 1000000007;
+// constexpr int MOD = 1000000009;
+
+// using mint = static_modint<MOD>;
+
+int main() {}
