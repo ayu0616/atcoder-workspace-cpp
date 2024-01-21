66ce350a5b6a07006a223525526c5494da567b16
ayu0616 <ayumuogawa0616@gmail.com>
2024-01-21 22:13:15 +0900

    典型90・021を解いた

--- typical90/021/main.cpp
+++ typical90/021/main.cpp
@@ -6,4 +6,26 @@
 
 // using mint = static_modint<MOD>;
 
-int main() {}
+int main() {
+    int N, M;
+    cin >> N >> M;
+    Graph g(N), g_rev(N);
+    rep(M) {
+        int a, b;
+        cin >> a >> b;
+        a--, b--;
+        g.add_edge(a, b);
+        g_rev.add_edge(b, a);
+    }
+
+    auto scc = g.scc(g_rev);
+
+    ll ans = 0;
+    rep(i, N) {
+        if (scc.root(i) != i) continue;
+        ll siz = scc.size(i);
+        ans += siz * (siz - 1LL) / 2LL;
+    }
+
+    cout << ans << endl;
+}
