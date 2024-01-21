dd1487c8b0d3006bdabb482e7a1af80a6baa2312
ayu0616 <ayumuogawa0616@gmail.com>
2024-01-21 22:13:15 +0900

    典型90・001を解いた

--- typical90/001/main.cpp
+++ typical90/001/main.cpp
@@ -6,4 +6,30 @@
 
 // using mint = static_modint<MOD>;
 
-int main() {}
+int main() {
+    int N, L, K;
+    cin >> N >> L >> K;
+    vector<int> A(N);
+    cin >> A;
+    A.push_back(L);
+    int l = 1, r = L+1, mid;
+
+    while (r - l > 1) {
+        mid = (l + r) / 2;
+        int prev = 0, cnt = 0;
+        rep(i, N + 1) {
+            int cur = A[i];
+            if (cur - prev >= mid) {
+                cnt++;
+                prev = A[i];
+            }
+        }
+        if (cnt < K + 1) {
+            r = mid;
+        } else {
+            l = mid;
+        }
+    }
+
+    cout << l << endl;
+}
