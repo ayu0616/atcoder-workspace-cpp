f7a9f7193a99fef4db43f8c58dd8f9fc469287bd
ayu0616 <ayumuogawa0616@gmail.com>
2024-01-21 22:13:15 +0900

    典型90・026を解いた

--- typical90/026/main.cpp
+++ typical90/026/main.cpp
@@ -6,4 +6,38 @@
 
 // using mint = static_modint<MOD>;
 
-int main() {}
+int main() {
+    int N;
+    cin >> N;
+    Graph g(N);
+    rep(i, N - 1) {
+        int A, B;
+        cin >> A >> B;
+        A--;
+        B--;
+        g.add_edge(A, B);
+        g.add_edge(B, A);
+    }
+    set<int> odd, even;
+    vb visited(N, false);
+
+    function<void(int, int)> dfs;
+    dfs = [&](int v, int depth) {
+        visited[v] = true;
+        if (depth % 2 == 0)
+            even.insert(v);
+        else
+            odd.insert(v);
+        for (auto e : g[v]) {
+            int u = e.to->id;
+            if (!visited[u]) dfs(u, depth + 1);
+        }
+    };
+
+    dfs(0, 0);
+    if (odd.size() < even.size()) swap(odd, even);
+    rep(i, N / 2) {
+        cout << *odd.begin() + 1 << " \n"[i == N / 2 - 1];
+        odd.erase(odd.begin());
+    }
+}
