dcbc3f1bc25e908a3292ea1adaf5be56bb25cae6
ayu0616 <ayumuogawa0616@gmail.com>
2024-01-21 22:13:15 +0900

    典型90・029を解いた

--- typical90/029/main.cpp
+++ typical90/029/main.cpp
@@ -6,4 +6,19 @@
 
 // using mint = static_modint<MOD>;
 
-int main() {}
+int main() {
+    int W, N;
+    cin >> W >> N;
+
+    SegTree st(W);
+    st.update(0, W, 0);
+
+    rep(i, N) {
+        int l, r;
+        cin >> l >> r;
+        l--, r--;
+        int tmp = st.query(l, r + 1);
+        st.update(l, r + 1, tmp - 1);
+        cout << -st.query(l, r + 1) << endl;
+    }
+}
