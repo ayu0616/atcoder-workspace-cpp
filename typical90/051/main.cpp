1e00e996449faf8289e0db77804a201af8e1496d
ayu0616 <ayumuogawa0616@gmail.com>
2024-01-27 11:52:26 +0900

    典型90を進めた

--- typical90/051/main.cpp
+++ typical90/051/main.cpp
@@ -6,4 +6,46 @@
 
 // using mint = static_modint<MOD>;
 
-int main() {}
+int main() {
+    int N, K;
+    ll P;
+    cin >> N >> K >> P;
+    vl A(N);
+    cin >> A;
+
+    int M = N / 2;
+    vl L = vl(A.begin(), A.begin() + M), R = vl(A.begin() + M, A.end());
+    vvl L_sum(K + 1), R_sum(K + 1);
+    rep(S, 1LL << M) {
+        ll sum = 0, cnt = 0;
+        rep(i, M) {
+            if (S >> i & 1) {
+                sum += L[i];
+                cnt++;
+            }
+        }
+        if (cnt <= K) L_sum[cnt].emplace_back(sum);
+    }
+    rep(S, 1LL << (N - M)) {
+        ll sum = 0, cnt = 0;
+        rep(i, (N - M)) {
+            if (S >> i & 1) {
+                sum += R[i];
+                cnt++;
+            }
+        }
+        if (cnt <= K) R_sum[cnt].emplace_back(sum);
+    }
+    rep(i, K + 1) {
+        sort(all(L_sum[i]));
+        sort(all(R_sum[i]));
+    }
+    ll ans = 0;
+    rep(i, K + 1) {
+        for (ll l : L_sum[i]) {
+            auto it = upper_bound(all(R_sum[K - i]), P - l);
+            ans += it - R_sum[K - i].begin();
+        }
+    }
+    cout << ans << endl;
+}
--- typical90/056/main.cpp
+++ typical90/056/main.cpp
@@ -6,4 +6,37 @@
 
 // using mint = static_modint<MOD>;
 
-int main() {}
+int main() {
+    int N, S;
+    cin >> N >> S;
+    vi A(N), B(N);
+    rep(i, N) cin >> A[i] >> B[i];
+    vvi dp(N + 1, vi(S + 1, 0));
+    vs prev_choices(S + 1), cur_choices(S + 1);
+    dp[0][0] = 1;
+    rep(i, N) {
+        rep(s, S) {
+            if (s + A[i] <= S) {
+                if (dp[i + 1][s + A[i]]) continue;
+                dp[i + 1][s + A[i]] |= dp[i][s];
+                if (dp[i + 1][s + A[i]]) {
+                    cur_choices[s + A[i]] = prev_choices[s] + "A";
+                }
+            }
+            if (s + B[i] <= S) {
+                if (dp[i + 1][s + B[i]]) continue;
+                dp[i + 1][s + B[i]] |= dp[i][s];
+                if (dp[i + 1][s + B[i]]) {
+                    cur_choices[s + B[i]] = prev_choices[s] + "B";
+                }
+            }
+        }
+        prev_choices = cur_choices;
+        cur_choices = vs(S + 1);
+    }
+    if (dp[N][S]) {
+        cout << prev_choices[S] << endl;
+    } else {
+        cout << "Impossible" << endl;
+    }
+}
