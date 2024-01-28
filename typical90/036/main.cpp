daaf7f41e5b38cf7402bde2462e75d82a407ba3c
ayu0616 <ayumuogawa0616@gmail.com>
2024-01-28 19:38:53 +0900

    精進精進

--- typical90/036/main.cpp
+++ typical90/036/main.cpp
@@ -6,4 +6,24 @@
 
 // using mint = static_modint<MOD>;
 
-int main() {}
+int main() {
+    ll N, Q;
+    cin >> N >> Q;
+    vl x(N), y(N);
+    rep(i, N) cin >> x[i] >> y[i];
+    vl x1(N), y1(N);
+    rep(i, N) {
+        x1[i] = x[i] + y[i];
+        y1[i] = x[i] - y[i];
+    }
+    ll x_min = min(x1), x_max = max(x1);
+    ll y_min = min(y1), y_max = max(y1);
+    while (Q--) {
+        int q;
+        cin >> q;
+        q--;
+        ll cx = x1[q], cy = y1[q];
+        ll ans = max(abs(cx - x_min), abs(cx - x_max), abs(cy - y_min), abs(cy - y_max));
+        cout << ans << endl;
+    }
+}
--- typical90/037/main.cpp
+++ typical90/037/main.cpp
@@ -6,4 +6,28 @@
 
 // using mint = static_modint<MOD>;
 
-int main() {}
+int main() {
+    int W, N;
+    cin >> W >> N;
+    vi L(N), R(N), V(N);
+    rep(i, N) cin >> L[i] >> R[i] >> V[i];
+    vvl dp(N + 1, vl(W + 2, 1));
+    dp[0][0] = 0;
+    vector<SegTree> st(N + 1, SegTree(W + 1));
+    rep(i, N + 1) st[i].update(0, W + 1, 1);
+    rep(i, N + 1) st[i].update(0, 1, 0);
+    rep(i, 1, N + 1) {
+        dp[i] = dp[i - 1];
+        rep(j, W + 1) {
+            int cl = max(0, j - R[i-1]), cr = max(0, j - L[i-1] + 1);
+            if (cl == cr) continue;
+            ll val = st[i-1].query(cl, cr);
+            if (val != 1) {
+                chmin(dp[i][j], val - V[i-1]);
+            }
+        }
+        rep(j, W + 1) st[i].update(j, j + 1, dp[i][j]);
+    }
+    ll ans = dp[N][W];
+    cout << -ans << endl;
+}
