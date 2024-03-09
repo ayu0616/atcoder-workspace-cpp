ebd2a002468b2be7876ed31e6f28bac10c8a963a
ayu0616 <ayumuogawa0616@gmail.com>
2024-03-09 18:21:20 +0900

    atcoder libraryを利用できるよう設定した

--- typical90/002/main.cpp
+++ typical90/002/main.cpp
@@ -1,4 +1,8 @@
+#include <atcoder/all>
+
 #include "../../lib.hpp"
+
+using namespace atcoder;
 
 // constexpr int MOD = 998244353;
 // constexpr int MOD = 1000000007;
@@ -6,4 +10,35 @@
 
 // using mint = static_modint<MOD>;
 
-int main() {}
+int main() {
+    ll n;
+    cin >> n;
+    constexpr char bracket[] = {'(', ')'};
+    rep(i, 1LL << n) {
+        string s;
+        rep(j, n) {
+            if (i >> j & 1) {
+                s += bracket[1];
+            } else {
+                s += bracket[0];
+            }
+        }
+        reverse(all(s));
+        stack<char> st;
+        bool ok = true;
+        rep(j, n) {
+            if (s[j] == bracket[0]) {
+                st.push(s[j]);
+            } else {
+                if (st.empty()) {
+                    ok = false;
+                    break;
+                }
+                st.pop();
+            }
+        }
+        if (ok && st.empty()) {
+            cout << s << endl;
+        }
+    }
+}
