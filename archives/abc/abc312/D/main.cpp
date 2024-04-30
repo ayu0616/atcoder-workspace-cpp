42571ebbd4401615f46900d251bf5958a8f33973
ayu0616 <ayumuogawa0616@gmail.com>
2024-04-30 15:27:59 +0900

    abc312 move to archives

--- archives/abc/abc312/D/main.cpp
+++ archives/abc/abc312/D/main.cpp
@@ -1,40 +1,39 @@
+#ifdef ONLINE_JUDGE
+#define NDEBUG
+#endif
+
 #include "../../lib.hpp"
 
-const int MOD = 998244353;
-// const int MOD = 1000000007;
-// const int MOD = 1000000009;
+constexpr int MOD = 998244353;
+// constexpr int MOD = 1000000007;
+// constexpr int MOD = 1000000009;
 
 using mint = static_modint<MOD>;
 
-int main()
-{
-    string s;
-    cin >> s;
-    vector<vector<mint>> dp(s.size(), vector<mint>(2));
-    if (s[0] == '(' || s[0] == '?')
-    {
-        dp[0][0] = 1;
-    }
-    else
-    {
-        dp[0][0] = 0;
-    }
-    dp[0][1] = 0;
-    rep(i, s.size() - 1)
-    {
-        if (s[i+1] == '(')
-        {
-            dp[i + 1][0] = dp[i][0] + dp[i][1];
-        }
-        else if (s[i+1] == ')')
-        {
-            dp[i + 1][1] = dp[i][0] + dp[i][1];
-        }
-        else
-        {
-            dp[i + 1][0] = dp[i][0] + dp[i][1];
-            dp[i + 1][1] = dp[i][0] + dp[i][1];
+int main() {
+    cout << fixed << setprecision(18);
+    string S;
+    cin >> S;
+    int N = S.size();
+    vector<vector<mint>> dp(N + 1, vector<mint>(N + 1, 0));
+    dp[0][0] = 1;
+    rep(i, N) {
+        if (S[i] == '(') {
+            rep(j, N) { dp[i + 1][j + 1] += dp[i][j]; }
+        } else if (S[i] == ')') {
+            rep(j, N) {
+                if (j > 0) {
+                    dp[i + 1][j - 1] += dp[i][j];
+                }
+            }
+        } else {
+            rep(j, N) {
+                dp[i + 1][j + 1] += dp[i][j];
+                if (j > 0) {
+                    dp[i + 1][j - 1] += dp[i][j];
+                }
+            }
         }
     }
-    cout << dp[s.size() - 1][1] << endl;
+    cout << dp[N][0] << endl;
 }
