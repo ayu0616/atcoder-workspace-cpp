9005c2563d0b3a697bdfd1c2fd7987f622fe0866
ayu0616 <ayumuogawa0616@gmail.com>
2024-05-04 00:02:45 +0900

    abc319 move to archives

--- archives/abc/abc319/C/main.cpp
+++ archives/abc/abc319/C/main.cpp
@@ -1,3 +1,7 @@
+#include <algorithm>
+#include <cassert>
+#include <vector>
+
 #include "../../lib.hpp"
 
 // const int MOD = 998244353;
@@ -6,8 +10,35 @@
 
 // using mint = static_modint<MOD>;
 
-int main()
-{
-    vvl c(3, vl(3));
-    rep(i, 3)rep(j, 3)cin>>c[i][j];
+int main() {
+    cout << fixed << setprecision(18);
+    vi c(9);
+    cin >> c;
+    vi vec = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+    double ans = 0;
+    double factorial_9 = 1;
+    for (int i = 1; i <= 9; i++) factorial_9 *= i;
+
+    vector<tuple<int, int, int>> cells = {
+        {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6},
+    };
+
+    do {
+        bool ok = true;
+        for (auto [x, y, z] : cells) {
+            if (c[x] == c[y] && vec[x] < vec[z] && vec[y] < vec[z]) {
+                ok = false;
+                break;
+            } else if (c[y] == c[z] && vec[y] < vec[x] && vec[z] < vec[x]) {
+                ok = false;
+                break;
+            } else if (c[z] == c[x] && vec[z] < vec[y] && vec[x] < vec[y]) {
+                ok = false;
+                break;
+            }
+        }
+        if (ok) ans++;
+    } while (next_permutation(all(vec)));
+
+    cout << ans / factorial_9 << endl;
 }
