82ace2933ab0d8510a91859b8457e06fa7e87aff
ayu0616 <ayumuogawa0616@gmail.com>
2024-05-03 17:41:20 +0900

    abc324 move to archives

--- archives/abc/abc324/C/main.cpp
+++ archives/abc/abc324/C/main.cpp
@@ -1,158 +1,46 @@
+#include <cstdlib>
+#ifdef ONLINE_JUDGE
+#define NDEBUG
+#endif
+
 #include "../../lib.hpp"
 
-// const int MOD = 998244353;
-// const int MOD = 1000000007;
-// const int MOD = 1000000009;
+// constexpr int MOD = 998244353;
+// constexpr int MOD = 1000000007;
+// constexpr int MOD = 1000000009;
 
 // using mint = static_modint<MOD>;
 
-bool c2(string s, string t)
-{
-    if (s.size() != t.size() + 1)
-        return false;
-    bool f = false;
-    rep(i, t.size())
-    {
-        if (f)
-        {
-            if (s[i] != t[i - 1])
-                return false;
-        }
-        else
-        {
-            if (s[i] != t[i])
-                f = true;
+string T_dash;
+
+bool judge(const string &S) {
+    if (S == T_dash) return true;
+    if (abs((int)S.size() - (int)T_dash.size()) > 1) return false;
+    int min_size = min(S.size(), T_dash.size());
+    int idx = -1;
+    rep(i, min_size) {
+        if (S[i] != T_dash[i]) {
+            idx = i;
+            break;
         }
     }
-    if (f && s.back() != t.back())
-        return false;
-    return true;
+    if (idx == -1 && S.substr(0, min_size) == T_dash.substr(0, min_size)) return true;
+    if (S.size() == T_dash.size() && S.substr(idx + 1) == T_dash.substr(idx + 1)) return true;
+    if (S.size() == T_dash.size() + 1 && S.substr(idx + 1) == T_dash.substr(idx)) return true;
+    if (S.size() == T_dash.size() - 1 && S.substr(idx) == T_dash.substr(idx + 1)) return true;
+    return false;
 }
 
-bool c3(string s, string t)
-{
-    if (s.size() + 1 != t.size())
-        return false;
-    bool f = false;
-    rep(i, s.size())
-    {
-        if (f)
-        {
-            if (s[i - 1] != t[i])
-                return false;
-        }
-        else
-        {
-            if (s[i] != t[i])
-                f = true;
-        }
-    }
-    if (f && s.back() != t.back())
-        return false;
-    return true;
-}
-
-bool c4(string s, string t)
-{
-    if (s.size() != t.size())
-        return false;
-    bool f = false;
-    rep(i, s.size())
-    {
-        if (f)
-        {
-            if (s[i] != t[i])
-                return false;
-        }
-        else
-        {
-            if (s[i] != t[i])
-                f = true;
-        }
-    }
-    return true;
-}
-
-map<pair<string, string>, ll> memo;
-ll ld(string s, string t, ll now = 0)
-{
-    if (now >= 5)
-        return memo[{s, t}] = 5;
-    if (memo.count({s, t}) > 0)
-        return memo[{s, t}];
-    if (s.empty())
-        return memo[{s, t}] = t.size();
-    if (t.empty())
-        return memo[{s, t}] = s.size();
-    ll res = ld(s.substr(1), t.substr(1)) + (s[0] != t[0]);
-    res = min(res, ld(s.substr(1), t, res) + 1);
-    res = min(res, ld(s, t.substr(1), res) + 1);
-    return memo[{s, t}] = res;
-}
-
-int main()
-{
+int main() {
+    cout << fixed << setprecision(18);
     ll N;
-    string T;
-    cin >> N >> T;
-    vector<string> S(N);
-    rep(i, N) cin >> S[i];
-    ll ans = 0;
-    vl ans_arr;
-
-    // set<string> tSet;
-
-    // tSet.insert(T);
-    // rep(i, T.size() + 1)
-    // {
-    //     for (char c = 'a'; c <= 'z'; c++)
-    //     {
-    //         tSet.insert(T.substr(0, i) + c + T.substr(i));
-    //     }
-    // }
-    // rep(i, T.size())
-    // {
-    //     tSet.insert(T.substr(0, i) + T.substr(i + 1));
-    //     for (char c = 'a'; c <= 'z'; c++)
-    //     {
-    //         tSet.insert(T.substr(0, i) + c + T.substr(i + 1));
-    //     }
-    // }
-
-    rep(i, N)
-    {
-        // if (S[i] == T)
-        // {
-        //     ans++;
-        //     ans_arr.push_back(i + 1);
-        // }
-        // else if (c2(S[i], T))
-        // {
-        //     ans++;
-        //     ans_arr.push_back(i + 1);
-        // }
-        // else if (c3(S[i], T))
-        // {
-        //     ans++;
-        //     ans_arr.push_back(i + 1);
-        // }
-        // else if (c4(S[i], T))
-        // {
-        //     ans++;
-        //     ans_arr.push_back(i + 1);
-        // }
-        // if (tSet.count(S[i]) > 0)
-        // {
-        //     ans++;
-        //     ans_arr.push_back(i + 1);
-        // }
-        if (ld(S[i], T) <= 1)
-        {
-            ans++;
-            ans_arr.push_back(i + 1);
-        }
+    cin >> N >> T_dash;
+    vs S(N);
+    cin >> S;
+    vi ans;
+    rep(i, N) {
+        if (judge(S[i])) ans.push_back(i + 1);
     }
-    cout << ans << endl;
-    if (ans)
-        cout << ans_arr << endl;
+    cout << ans.size() << endl;
+    if (ans.size()) cout << ans << endl;
 }
