0f172d67589c740e4db5d39dcd887208026b50cd
ayu0616 <ayumuogawa0616@gmail.com>
2024-05-06 13:32:47 +0900

    abc317 move to archives

--- archives/abc/abc317/E/main.cpp
+++ archives/abc/abc317/E/main.cpp
@@ -1,3 +1,5 @@
+#include <queue>
+
 #include "../../lib.hpp"
 
 // const int MOD = 998244353;
@@ -6,6 +8,58 @@
 
 // using mint = static_modint<MOD>;
 
-int main()
-{
+constexpr char* way = ">v<^";
+
+int main() {
+    int H, W;
+    cin >> H >> W;
+    vs A(H);
+    cin >> A;
+    rep(h, H) rep(w, W) {
+        rep(way_i, 4) {
+            if (A[h][w] == way[way_i]) {
+                int nh = h + dy4[way_i];
+                int nw = w + dx4[way_i];
+                while (0 <= nh && nh < H && 0 <= nw && nw < W && (A[nh][nw] == '.' || A[nh][nw] == '!')) {
+                    A[nh][nw] = '!';
+                    nh += dy4[way_i];
+                    nw += dx4[way_i];
+                }
+            }
+        }
+    }
+
+    pii start, goal;
+    rep(h, H) rep(w, W) {
+        if (A[h][w] == 'S') {
+            start = {h, w};
+            A[h][w] = '.';
+        }
+        if (A[h][w] == 'G') {
+            goal = {h, w};
+            A[h][w] = '.';
+        }
+    }
+
+    vvi dist(H, vi(W, INT_INF));
+    queue<pii> q;
+    q.push(start);
+    dist[start.first][start.second] = 0;
+    while (!q.empty()) {
+        auto [h, w] = q.front();
+        q.pop();
+        rep(way_i, 4) {
+            int nh = h + dy4[way_i];
+            int nw = w + dx4[way_i];
+            if (0 <= nh && nh < H && 0 <= nw && nw < W && A[nh][nw] == '.' && dist[nh][nw] == INT_INF) {
+                dist[nh][nw] = dist[h][w] + 1;
+                q.push({nh, nw});
+            }
+        }
+    }
+    if (dist[goal.first][goal.second] == INT_INF) {
+        cout << -1 << endl;
+    } else {
+        cout << dist[goal.first][goal.second] << endl;
+    }
 }
