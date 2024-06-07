aeb749ed62a4144f0afd9f5698479728789242fb
ayu0616 <ayumuogawa0616@gmail.com>
2024-06-07 10:41:22 +0900

    abc270 move to archives

--- archives/abc/abc270/E/main.cpp
+++ archives/abc/abc270/E/main.cpp
@@ -1,3 +1,4 @@
+#include <queue>
 #ifdef ONLINE_JUDGE
 #define NDEBUG
 #endif
@@ -12,6 +13,43 @@
 
 int main() {
     cout << fixed << setprecision(18);
-    ll N;
-    cin >> N;
+    ll N, K;
+    cin >> N >> K;
+    vl A(N);
+    cin >> A;
+    priority_queue<ll, vl, greater<ll>> pq;
+    for (auto a : A) {
+        pq.push(a);
+    }
+    ll cur = 0;     // 現在までに食べたりんごの数
+    ll cycles = 0;  // 現在までの周回数
+    ll rest = N;    // 1こ以上りんごが存在するかごの数
+    while (cur < K) {
+        ll next_cycle = pq.top() - cycles;
+        ll next = cur + next_cycle * rest;
+        if (next > K) {
+            ll additional_cycles = (K - cur) / rest;
+            cycles += additional_cycles;
+            cur += additional_cycles * rest;
+            break;
+        }
+        cycles += next_cycle;
+        cur = next;
+        const ll min = pq.top();
+        while (pq.size() > 0 && pq.top() == min) {
+            pq.pop();
+            rest--;
+        }
+    }
+    vl ans;
+    for (auto a : A) {
+        a -= cycles;
+        chmax(a, 0LL);
+        if (cur < K && a > 0) {
+            cur++;
+            a--;
+        }
+        ans.push_back(a);
+    }
+    cout << ans << endl;
 }
