46a65ba651079d414fc06ef5b039276c13ec6428
ayu0616 <ayumuogawa0616@gmail.com>
2024-09-07 12:17:08 +0900

    abc354 move to archives

--- archives/abc/abc354/F/main.cpp
+++ archives/abc/abc354/F/main.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <iostream>
 #ifdef ONLINE_JUDGE
 #define NDEBUG
 #endif
@@ -10,8 +12,41 @@
 
 // using mint = static_modint<MOD>;
 
+vi lis(const vl &A) {
+    int N = A.size();
+    vl dp(N, LL_INF);
+    vi ret(N, 0);
+    rep(i, N) {
+        auto it = lower_bound(all(dp), A[i]);
+        *it = A[i];
+        ret[i] = it - dp.begin() + 1;
+    }
+    return ret;
+}
+
 int main() {
     cout << fixed << setprecision(18);
     ll T;
     cin >> T;
+    rep(t, T) {
+        int N;
+        cin >> N;
+        vl A(N);
+        cin >> A;
+        vi lis_front = lis(A);
+        reverse(all(A));
+        rep(i, N) { A[i] = -A[i]; }
+        vi lis_back = lis(A);
+        rep(i, N) { A[i] = -A[i]; }
+        reverse(all(lis_back));
+        const int lis_len = *max_element(all(lis_front));
+        vi ans;
+        rep(i, N) {
+            if (lis_front[i] + lis_back[i] - 1 == lis_len) {
+                ans.push_back(i+1);
+            }
+        }
+        cout << ans.size() << endl;
+        cout << ans << endl;
+    }
 }
