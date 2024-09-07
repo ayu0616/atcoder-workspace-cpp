8a38d1074d1ea916e363fd2a2f02fa9a80b985d1
ayu0616 <ayumuogawa0616@gmail.com>
2024-09-07 15:54:47 +0900

    abc353 move to archives

--- archives/abc/abc353/E/main.cpp
+++ archives/abc/abc353/E/main.cpp
@@ -1,3 +1,4 @@
+#include <functional>
 #ifdef ONLINE_JUDGE
 #define NDEBUG
 #endif
@@ -10,36 +11,32 @@
 
 // using mint = static_modint<MOD>;
 
-ll N;
-ll ans = 0;
-vs S;
-
-ll solve(vector<int> s, int depth) {
-    vector<int> groups[26];
-    for (int i : s) {
-        if (S[i].size() <= depth) continue;
-        groups[S[i][depth] - 'a'].push_back(i);
+bool prefix_compare(const string &a, const string &b) {
+    if (a.size() < b.size()) {
+        return a < b.substr(0, a.size());
+    } else {
+        return a.substr(0, b.size()) < b;
     }
-    ll dp = 0;
-    rep(i, 100) {
-        if (groups[i].size() >= 2) {
-            dp += solve(groups[i], depth + 1);
-        }
-    }
-    ll cnt_s = s.size();
-    if (cnt_s == 0) return dp;
-    ll n = cnt_s * (cnt_s - 1) / 2 - dp;
-    ans += n * depth;
-    return dp + n;
 }
 
 int main() {
     cout << fixed << setprecision(18);
+    ll N;
     cin >> N;
-    S.resize(N);
+    vs S(N);
     cin >> S;
-    vector<int> s(N);
-    iota(all(s), 0);
-    solve(s, 0);
+    sort(all(S));
+    ll ans = 0;
+    rep(i, N) {
+        int ub = S.size(), lb = i + 1;
+        string s = "";
+        rep(j, S[i].size()) {
+            s += S[i][j];
+            ub = upper_bound(S.begin() + lb, S.begin() + ub, s, prefix_compare) - S.begin();
+            lb = lower_bound(S.begin() + lb, S.begin() + ub, s, prefix_compare) - S.begin();
+            ans += ub - lb;
+            if (ub - lb == 0) break;
+        }
+    }
     cout << ans << endl;
 }
