f6dd4406430e2dd95b97b17acf72ea74b3e5255f
ayu0616 <ayumuogawa0616@gmail.com>
2024-09-07 11:17:00 +0900

    abc362 move to archives

--- archives/abc/abc362/G/main.cpp
+++ archives/abc/abc362/G/main.cpp
@@ -2,43 +2,42 @@
 #define NDEBUG
 #endif
 
+#include <atcoder/string.hpp>
+
 #include "../../lib.hpp"
 
-constexpr int MOD = 998244353;
+// constexpr int MOD = 998244353;
 // constexpr int MOD = 1000000007;
 // constexpr int MOD = 1000000009;
 
-using mint = static_modint<MOD>;
+// using mint = static_modint<MOD>;
+
+int binary_search(const vi& sa, const string& S, const string& T) {
+    int ng = -1, ok = sa.size();
+    while (ok - ng > 1) {
+        int mid = (ok + ng) / 2;
+        if (S.compare(sa[mid], T.size(), T) >= 0) {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+    return ok;
+}
 
 int main() {
     cout << fixed << setprecision(18);
     string S;
     ll Q;
     cin >> S >> Q;
-    ll N = S.size();
-    map<ll, multiset<ll>> memo;
+    auto sa = atcoder::suffix_array(S);
     while (Q--) {
         string T;
         cin >> T;
-        ll M = T.size();
-        mint hash = 0;
-        rep(i, M) { hash = hash * 26 + T[i] - 'a'; }
-        mint s = 0;
-        rep(i, M) { s = s * 26 + S[i] - 'a'; }
-        ll ans = 0;
-        if (memo.count(M)) {
-            cout << memo[M].count(hash.val()) << endl;
-            continue;
-        }
-        mint m_pow = mint(26).pow(M);
-        rep(i, N - M + 1) {
-            memo[M].insert(s.val());
-            if (s == hash) ++ans;
-            if (i + M < N) {
-                s = s * 26 + S[i + M] - 'a';
-                s -= mint(S[i] - 'a') * m_pow;
-            }
-        }
-        cout << ans << endl;
+        int l = binary_search(sa, S, T);
+        int r = binary_search(sa, S, T + char(127));
+        // debug(l);
+        // debug(r);
+        cout << r - l << endl;
     }
 }
