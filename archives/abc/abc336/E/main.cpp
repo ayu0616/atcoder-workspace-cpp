a6f598743e4f4a141615154eb4cc0ce62d746e06
ayu0616 <ayumuogawa0616@gmail.com>
2025-01-03 16:41:44 +0900

    abc336 move to archives

--- archives/abc/abc336/E/main.cpp
+++ archives/abc/abc336/E/main.cpp
@@ -1,3 +1,7 @@
+#ifdef ONLINE_JUDGE
+#define NDEBUG
+#endif
+
 #include "../../lib.hpp"
 
 // constexpr int MOD = 998244353;
@@ -5,39 +9,22 @@
 // constexpr int MOD = 1000000009;
 
 // using mint = static_modint<MOD>;
+ll ans = 0;
+string N;
+void solve(int s) {
+    vector dp(N.size() + 1, vector(s + 1, vector(s, vector(2, 0LL))));
+    dp[0][0][0][1] = 1;
+    rep(d, N.size()) rep(i, s + 1) rep(j, s) rep(f, 2) rep(t, 10) {
+        if (i + t > s) continue;
+        if (f && (t > N[d] - '0')) continue;
+        dp[d + 1][i + t][(j * 10 + t) % s][f && (t == N[d] - '0')] += dp[d][i][j][f];
+    }
+    rep(f, 2) ans += dp[N.size()][s][0][f];
+}
 
 int main() {
-    ll N;
+    cout << fixed << setprecision(18);
     cin >> N;
-    string S = to_string(N);
-    int L = log10(N) + 1;
-    int k_max = L * 9;
-    ll ans = 0;
-    rep(i, 1, k_max + 1) {
-        vector<vector<vvl>> dp(L + 1, vector<vvl>(k_max+1, vvl(k_max+1, vl(2, 0))));
-        dp[0][0][0][0] = 1;
-        rep(j, L) {
-            int D = S[j] - '0';
-            rep(k, k_max) {
-                rep(l, k_max) {
-                    rep(m, 2) {
-                        rep(d, 10) {
-                            int nj = j + 1;
-                            int nk = k + d;
-                            int nl = (l + d * (ll)powl(10, L - j - 1)) % i;
-                            int nm = m;
-                            if (d > D) continue;
-                            if (d < D) nm = 1;
-                            if (d == D) nm = m;
-                            if (nk > k_max) continue;
-                            if (nl > k_max) continue;
-                            dp[nj][nk][nl][nm] += dp[j][k][l][m];
-                        }
-                    }
-                }
-            }
-        }
-        ans += dp[L][i][0][0] + dp[L][i][0][1];
-    }
+    rep(s, 1, 9 * 14 + 1) { solve(s); }
     cout << ans << endl;
 }
