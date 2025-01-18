514e6319ed1c932ba3157fc4ebd9b6ad75df278e
ayu0616 <ayumuogawa0616@gmail.com>
2025-01-18 20:51:23 +0900

    abc385 move to archives

--- archives/abc/abc385/E/main.cpp
+++ archives/abc/abc385/E/main.cpp
@@ -14,56 +14,26 @@ int main() {
     cout << fixed << setprecision(18);
     ll N;
     cin >> N;
-    Graph g(N);
+    vvi g(N);
     rep(i, N - 1) {
-        ll a, b;
-        cin >> a >> b;
-        a--, b--;
-        g.add_edge(a, b);
-        g.add_edge(b, a);
+        ll A, B;
+        cin >> A >> B;
+        A--;
+        B--;
+        g[A].push_back(B);
+        g[B].push_back(A);
     }
-    set<ll> level1, level2, level3;
-    vb visited(N, false);
-    rep(i, N) {
-        if (g[i].size() == 1) {
-            visited[i] = true;
-            visited[g[i][0]] = true;
-            for (auto l2 : g[g[i][0]]) {
-                if(!visited[l2]) {
-                    level3.insert(l2);
-                    visited[l2] = true;
-                }
-            }
+    ll ans = 0;
+    rep(v, N) {
+        vl d;
+        for (auto u : g[v]) {
+            int deg = g[u].size();
+            if (deg == 1) continue;
+            d.push_back(deg - 1);
         }
+        sort(all(d));
+        reverse(all(d));
+        rep(j, d.size()) { chmax(ans, d[j] * (j + 1) + (j + 1) + 1); }
     }
-    ll ans = LL_INF;
-    {
-        ll max_reg = 0;
-        rep(i, N) {
-            if (max_reg < g[i].size()) {
-                max_reg = g[i].size();
-            }
-        }
-        chmin(ans, N - max_reg - 1);
-    }
-    for (auto l3 : level3) {
-        ll c = 0;
-        map<ll, ll> mp;
-        set<ll> se;
-        for (auto l2 : g[l3]) {
-            if (g[l2].size() > 1) {
-                se.insert(g[l2].size());
-                mp[g[l2].size()]++;
-            }
-        }
-        ll cnt = 0;
-        auto it = se.rbegin();
-        while (it != se.rend()) {
-            cnt += mp[*it];
-            chmax(c, cnt * (*it));
-            it++;
-        }
-        chmin(ans, N - c - 1);
-    }
-    cout << ans << endl;
+    cout << N - ans << endl;
 }
