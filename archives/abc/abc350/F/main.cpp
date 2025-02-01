0c860dc6f501dff3a5ead45791c7053192503d14
ayu0616 <ayumuogawa0616@gmail.com>
2025-02-01 16:36:04 +0900

    abc350 move to archives

--- archives/abc/abc350/F/main.cpp
+++ archives/abc/abc350/F/main.cpp
@@ -10,8 +10,48 @@
 
 // using mint = static_modint<MOD>;
 
+char switch_upper_lower(char c) {
+    if ('a' <= c && c <= 'z') {
+        return c - 'a' + 'A';
+    } else if ('A' <= c && c <= 'Z') {
+        return c - 'A' + 'a';
+    } else {
+        return c;
+    }
+}
+
 int main() {
     cout << fixed << setprecision(18);
-    int N;
-    cin >> N;
+    string S;
+    cin >> S;
+    int N = S.size();
+    vi pair(N, -1);
+    vi st;
+    rep(i, N) {
+        if (S[i] == '(') {
+            st.push_back(i);
+        } else if (S[i] == ')') {
+            int j = st.back();
+            st.pop_back();
+            pair[i] = j;
+            pair[j] = i;
+        }
+    }
+    bool rev = false;
+    string ans = "";
+    int i = 0;
+    while (i < N) {
+        if (S[i] == '(' || S[i] == ')') {
+            i = pair[i];
+            rev = !rev;
+        } else {
+            ans += rev ? switch_upper_lower(S[i]) : S[i];
+        }
+        if (rev) {
+            i--;
+        } else {
+            i++;
+        }
+    }
+    cout << ans << endl;
 }
